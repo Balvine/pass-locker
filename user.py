54acf851573c78e18f8a32fc789e6bfb5673ee68
Balvine <balvy075@gmail.com>
2020-09-06 21:52:34 +0300

    added remove  and append method

---
+++ user.py
@@ -0,0 +1,28 @@
+class user:
+  """
+ class that will generate new users
+"""
+  user_detail = []
+
+def save_detail(self)
+     
+
+
+   """
+    append()	Adds an element at the end of the list
+   """
+   
+
+User.user_detail.append(self)
+
+
+def delete_detail():
+
+
+    """"
+     remove()	Removes the item with the specified value
+
+    this method is used to remove objects fom the user detail array
+
+    """"
+    User.user_detail.remove(self)
