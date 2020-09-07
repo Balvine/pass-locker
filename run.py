f18c6c8ca6eb05e614a3f5c70415b1de2c1abd66
Balvine <balvy075@gmail.com>
2020-09-07 13:49:52 +0300

    created a user account

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.6
+from user import User
+import getpass
+import random
+import string
+dash = '-' * 60
+
+def create_account(account_name,username,password,confirm_password):
+    '''
+    Function to create a new user
+    '''
+    new_user =  User(account_name,username,password,confirm_password)
+
+    return new user
+
+def save_details(user):
+
+    '''
+    Function to save save_details
+    '''
+    user.save_detail():
