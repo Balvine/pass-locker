e06a60c35f2ad2b80ebd91f4c35d47068c75c1be
Balvine <balvy075@gmail.com>
2020-09-07 12:17:17 +0300

    added credential test class

---
+++ test_credentials.py
@@ -0,0 +1,9 @@
+import unittest
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+    '''
